2a410916a86328a00ae2b25c4ac13a91ecf052f1
chris-sun-star <hongxin.sun.vip@gmail.com>
2025-06-10 17:50:28 +0800

    update package hash

--- Formula/oceanbase-ce.rb
+++ Formula/oceanbase-ce.rb
@@ -1,17 +1,14 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class OceanbaseCe < Formula
   desc "A wrapper to run oceanbase-ce using docker containers"
   homepage "https://github.com/chris-sun-star/oceanbase-ce"
   url "https://github.com/chris-sun-star/oceanbase-ce/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "d0bd24755bda21861fbf80241a6a002cff9b43d576f1c1c02b6f76eb87e91b45"
+  sha256 "5bbe48d0b94418ab55406245b6a37f4c9f92dd26c45251adbbf0da2aa90cb6a1"
   license "Apache-2.0"
 
   depends_on "docker"
+
   def install
     bin.install "src/oceanbase-ce.sh" => "oceanbase-ce"
-    # docker pull oceanbase/oceanbase-ce
     system "docker", "pull", "oceanbase-ce"
   end
 end
